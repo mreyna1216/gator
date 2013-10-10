6f4705753bf6ede126239aa56eb518dfc4397b11
Jon Medhurst <tixy@linaro.org>
2013-10-10 19:31:31 +0100

    gator-driver: Fix building on Linux 3.12
    In Linux 3.12 the local timer API was removed by commit 060fd304
    (ARM: smp: Remove local timer API) so disable the check for this as it
    stops compilation.
    
    We need the check for kernels before 3.12 because otherwise we would
    get incorrect timing information on A5/A9.
    
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- driver/gator_main.c
+++ driver/gator_main.c
@@ -44,8 +44,10 @@ static unsigned long gator_protocol_version = 16;
 #error gator requires the kernel to have CONFIG_HIGH_RES_TIMERS defined to support PC sampling
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 12, 0) /* Because CONFIG_LOCAL_TIMERS was removed in 3.12 */
 #if defined(__arm__) && defined(CONFIG_SMP) && !defined(CONFIG_LOCAL_TIMERS)
 #error gator requires the kernel to have CONFIG_LOCAL_TIMERS defined on SMP systems
+#endif
 #endif
 
 #if (GATOR_PERF_SUPPORT) && (!(GATOR_PERF_PMU_SUPPORT))
